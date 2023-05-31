ed91e0b1c23aa3ed968f8ecb5b29686c8d227b44
jeffryc6 <jeffryc6@outlook.com>
2023-05-30 19:25:14 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const { createBot, createProvider, createFlow } = require('@bot-whatsapp/bot')
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+// Importamos los flujos de conversación desde el archivo messages.js
+const { flowPrincipal } = require('./messages')
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
---
+++ messages.js
@@ -0,0 +1,42 @@
+const { addKeyword } = require('@bot-whatsapp/bot')
+
+const { addKeyword } = require('@bot-whatsapp/bot')
+const Agenda = require('agenda')
+
+// Configura la conexión a la base de datos para Agenda
+const agenda = new Agenda({ db: { address: 'mongodb://localhost/agenda' } })
+
+// Define una tarea de agenda para programar una cita
+agenda.define('schedule appointment', (job, done) => {
+  const { service, time } = job.attrs.data
+  console.log(`Scheduled appointment for ${service} at ${time}`)
+  done()
+})
+
+const flowService = addKeyword(['1', '2']).addAnswer((message) => {
+  const service = message.body === '1' ? 'Barba' : 'Corte de cabello'
+  const price = message.body === '1' ? '6000 colones' : '5000 colones'
+  return `Has seleccionado ${service} que tiene un costo de ${price}. Por favor, selecciona un horario para tu cita.`
+})
+
+const flowSchedule = addKeyword(['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00']).addAnswer((message) => {
+  const time = message.body
+  // Programa la cita utilizando Agenda
+  agenda.schedule(time, 'schedule appointment', { service: flowService, time })
+  return `Tu cita ha sido agendada para las ${time}. ¡Te esperamos!`
+})
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+  .addAnswer('🙌 Hola, bienvenido a nuestra Barbería. ¿Qué servicio te gustaría agendar hoy?')
+  .addAnswer(
+    [
+      '1. Barba - 6000 colones',
+      '2. Corte de cabello - 5000 colones',
+    ],
+    null,
+    null,
+    [flowService]
+  )
+
+//Exporta el flujo de la aplicacion para que pueda ser utilizados por otros archivos
+  module.exports = { flowPrincipal }
